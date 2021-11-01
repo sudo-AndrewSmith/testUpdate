45c443ea0d54dbf5502e953df06a96fa5f1711ab
Andrew <logopedd85@gmail.com>
2021-11-01 13:04:52 +0200

    Update App.js

--- AutoUpdateApp/App.js
+++ AutoUpdateApp/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 import codePush from 'react-native-code-push';
 
-let codePushOptions = {checkFrequency: codePush.CheckFrequency.MANUAL};
+let codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.MANUAL,
+};
 
 let App = () => {
+  const [status, setStatus] = useState('');
+
+  useEffect(() => {
+    codePush.checkForUpdate().then(update => {
+      if (!update) {
+        setStatus('обновлено!');
+      } else {
+        setStatus('Есть обнова!');
+      }
+    });
+  }),
+    [codePush];
+
   const handleCheckUpdates = () => {
     codePush.sync({
       updateDialog: true,
@@ -13,13 +28,18 @@ let App = () => {
     });
   };
 
+  useEffect(() => {
+    codePush.notifyAppReady(message => console.log(message));
+  }, []);
+
   return (
     <View style={{flex: 1}}>
-      <Text>pizda</Text>
+      <Text style={{textAlign: 'center', fontSize: 50, margin: 10}}>pizda</Text>
       <View style={styles.container}>
         <TouchableOpacity style={styles.button} onPress={handleCheckUpdates}>
           <Text style={styles.text}>Check for updates bigom</Text>
         </TouchableOpacity>
+        <Text style={styles.textStatus}>{status}</Text>
       </View>
     </View>
   );
@@ -41,6 +61,11 @@ const styles = StyleSheet.create({
     margin: 10,
     fontSize: 20,
     color: '#fff',
+  },
+  textStatus: {
+    margin: 10,
+    fontSize: 20,
+    color: 'red',
   },
 });
 
